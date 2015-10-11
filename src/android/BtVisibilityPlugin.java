9ba7def2273b582de5ca7acd7ab9dc6bb1662aa2
Joshua Yuan <joshua43210@gmail.com>
2015-10-11 00:39:32 -0700

    billy doesnt know how to commit with messages apparently

--- src/android/BtVisibilityPlugin.java
+++ src/android/BtVisibilityPlugin.java
@@ -25,6 +25,10 @@ import android.view.View;
 import android.view.Menu;
 import android.os.Bundle;
 
+import android.content.IntentFilter;
+import android.content.BroadcastReceiver;
+
+
 /**
  * The Class MacAddressPlugin.
  */
@@ -83,20 +87,47 @@ public class BtVisibilityPlugin extends CordovaPlugin {
     private boolean enableBtVisibility( ) {
         // Context oContext = mContext;
 
-        // BluetoothAdapter ba =  BluetoothAdapter.getDefaultAdapter();
-        // Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE); //request user to turn on
-        // discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 180); //extends it
-        // Context.startActivity(discoverableIntent); //does it
+        // // BluetoothAdapter ba =  BluetoothAdapter.getDefaultAdapter();
+        // // Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE); //request user to turn on
+        // // discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 180); //extends it
+        // // Context.startActivity(discoverableIntent); //does it
         
         // if(D) Log.d(TAG, "ensure discoverable");
-         BluetoothAdapter ba = BluetoothAdapter.getDefaultAdapter();
-            if (ba.getScanMode() !=
-                    ba.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
-                Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-                discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
-                startActivity(discoverableIntent);
-            }
+        BluetoothAdapter ba = BluetoothAdapter.getDefaultAdapter();
+        //     if (ba.getScanMode() !=
+        //             ba.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+        //         Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        //         discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        //         startActivity(discoverableIntent);
+        // }
 
-        return true;
+        // return true;
+        IntentFilter filter = new IntentFilter();
+
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+
+        registerReceiver(mReceiver, filter);
+        ba.startDiscovery();
+
+
+
     }
+        private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
+                //discovery starts, we can show progress dialog or perform other tasks
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                //discovery finishes, dismis progress dialog
+            } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                //bluetooth device found
+                BluetoothDevice device = (BluetoothDevice) intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                showToast("Found device " + device.getName());
+            }
+        }
+    };
 }
